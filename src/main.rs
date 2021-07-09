23952fc190800f75aea8bb30aef2d5675d175946
Rob <rm.rf.etc@gmail.com>
2021-07-08 17:13:19 -0700

    Element-wise multiplication

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,13 @@ extern crate rand;
 mod matrix2d;
 mod ml;
 
+use ndarray::{Array, Array2};
+
 fn main() {
     println!("sigmoid of -1: {:?}", ml::sigmoid(-1.0));
     println!("relu of -1: {:?}", ml::relu(-1.0));
-    println!("matrix of 3x2: {:?}", matrix2d::new2d_rand(3, 2, 0.01));
+    let m1 = matrix2d::new_rand(3, 2) * 0.01;
+    println!("matrix of 3x2:\n{:?}", m1);
+    let m2: Array2<f32> = Array::zeros((3, 1));
+    println!("element-wise multiplication:\n{:?}", m1 * m2);
 }
--- src/matrix2d.rs
+++ src/matrix2d.rs
@@ -1,9 +1,9 @@
 use ndarray::{Array, Array2};
 use rand::prelude::*;
 
-pub fn new2d_rand(r: usize, c: usize, scale: f32) -> Array2<f32> {
+pub fn new_rand(r: usize, c: usize) -> Array2<f32> {
     let mut rng = rand::thread_rng();
-    Array::from_shape_fn((r, c), |_| rng.gen::<f32>() * scale)
+    Array::from_shape_fn((r, c), |_| rng.gen::<f32>())
 }
 
 #[cfg(test)]
@@ -11,8 +11,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_new2d_rand() {
-        let m = new2d_rand(2, 2, 0.01);
+    fn test_new_rand() {
+        let m = new_rand(2, 2) * 0.01;
         assert_eq!(m.len(), 4);
         assert_eq!(m.shape(), [2, 2]);
         assert!(m[[0, 0]] <= 0.01 && m[[0, 0]] >= 0.0);
