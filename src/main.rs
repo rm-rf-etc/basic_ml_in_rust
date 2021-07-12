3b2297ad3e3575c76bcb4687bff1fa9adc3bddaf
Rob <rm.rf.etc@gmail.com>
2021-07-12 13:18:46 -0700

    Add function ml::init_2l_nn

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,23 @@ mod ml;
 use ndarray::{Array, Array2};
 
 fn main() {
-    println!("sigmoid of -1: {:?}", ml::sigmoid(-1.0));
-    println!("relu of -1: {:?}", ml::relu(-1.0));
-    let m1 = matrix2d::new_rand(3, 2) * 0.01;
-    println!("matrix of 3x2:\n{:?}", m1);
-    let m2: Array2<f32> = Array::zeros((3, 1));
-    println!("element-wise multiplication:\n{:?}", m1 * m2);
+    println!("\nsigmoid of -1: {:?}", ml::sigmoid(-1.0));
+
+    println!("\nrelu of -1: {:?}", ml::relu(-1.0));
+
+    let m1 = matrix2d::new_rand(3, 2);
+    println!("\nmatrix of 3x2:\n{:?}", m1);
+
+    let m2 = m1 * 0.01;
+    println!("\nscalar multiplication:\n{:?}", m2);
+
+    let m3: Array2<f32> = Array::zeros((3, 1));
+    println!("\nelement-wise multiplication:\n{:?}", m2 * m3);
+
+    let nn1 = ml::init_2l_nn(3, 2, 1).unwrap();
+    println!("\n2L NN:");
+    println!("\nW1:\n{:?}", nn1["W1"]);
+    println!("\nb1:\n{:?}", nn1["b1"]);
+    println!("\nW2:\n{:?}", nn1["W2"]);
+    println!("\nb2:\n{:?}", nn1["b2"]);
 }
--- src/ml.rs
+++ src/ml.rs
@@ -1,11 +1,38 @@
+use super::matrix2d;
+use ndarray::Array2;
+use std::collections::HashMap;
 use std::f32::consts::E;
 
+// sigmoid activation function
 pub fn sigmoid(f: f32) -> f32 {
     1.0 / (1.0 + E.powf(-f))
 }
 
+// relu activation function
 pub fn relu(f: f32) -> f32 {
     f32::max(0.0, f)
+}
+
+// This function generates a 2 layer NN. We will later improve this function,
+// making it more generic, so we can init an arbitrary number of layers.
+pub fn init_2l_nn(
+    input_size: usize,
+    hidden_size: usize,
+    output_size: usize,
+) -> Result<HashMap<String, Array2<f32>>, String> {
+    let w1 = matrix2d::new_rand(hidden_size, input_size);
+    let b1: Array2<f32> = Array2::zeros((hidden_size, 1));
+    let w2 = matrix2d::new_rand(output_size, hidden_size);
+    let b2: Array2<f32> = Array2::zeros((output_size, 1));
+
+    let mut map = HashMap::new();
+
+    map.insert("W1".to_string(), w1);
+    map.insert("b1".to_string(), b1);
+    map.insert("W2".to_string(), w2);
+    map.insert("b2".to_string(), b2);
+
+    Ok(map)
 }
 
 #[cfg(test)]
@@ -24,5 +51,14 @@ mod tests {
         assert_eq!(relu(0.0), 0.0);
         assert_eq!(relu(1.0), 1.0);
         assert_eq!(relu(-1.0), 0.0);
+    }
+
+    #[test]
+    fn test_init_2l_nn() {
+        let nn1 = init_2l_nn(3, 2, 1).unwrap();
+        assert_eq!(nn1["W1"].shape(), [2, 3]);
+        assert_eq!(nn1["b1"].shape(), [2, 1]);
+        assert_eq!(nn1["W2"].shape(), [1, 2]);
+        assert_eq!(nn1["b2"].shape(), [1, 1]);
     }
 }
