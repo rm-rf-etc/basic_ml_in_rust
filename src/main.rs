8b290bed3c88ba729e7d4c9c51d52e420ed1ad48
Rob <rm.rf.etc@gmail.com>
2021-07-12 18:27:15 -0700

    Add linear_forward

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate rand;
 mod matrix2d;
 mod ml;
 
-use ndarray::{Array, Array2};
+use ndarray::Array2;
 
 fn main() {
     println!("\nsigmoid of -1: {:?}", ml::sigmoid(-1.0));
@@ -15,7 +15,7 @@ fn main() {
     let m2 = m1 * 0.01;
     println!("\nscalar multiplication:\n{:?}", m2);
 
-    let m3: Array2<f32> = Array::zeros((3, 1));
+    let m3 = Array2::<f32>::zeros((3, 1));
     println!("\nelement-wise multiplication:\n{:?}", m2 * m3);
 
     let nn1 = ml::init_deep_nn_params(vec![3, 2, 2, 1]).unwrap();
@@ -26,4 +26,19 @@ fn main() {
     println!("\nb2:\n{:?}", nn1["b2"]);
     println!("\nW2:\n{:?}", nn1["W3"]);
     println!("\nb2:\n{:?}", nn1["b3"]);
+
+    let m1 = ndarray::arr2(&[
+        [1.62434536, -0.61175641],
+        [-0.52817175, -1.07296862],
+        [0.86540763, -2.3015387],
+    ]);
+    let m2 = ndarray::arr2(&[[1.74481176, -0.7612069, 0.3190391]]);
+    let b = ndarray::arr2(&[[-0.24937038]]);
+    let (z, _) = ml::linear_forward(m1, m2, b);
+    let expected_out = ndarray::arr2(&[[3.26295337, -1.23429987]]);
+
+    println!("{:?}", z[[0, 0]]);
+    println!("{:?}", expected_out[[0, 0]]);
+    println!("{:?}", z[[0, 1]]);
+    println!("{:?}", expected_out[[0, 1]]);
 }
--- src/ml.rs
+++ src/ml.rs
@@ -3,6 +3,9 @@ use ndarray::Array2;
 use std::collections::HashMap;
 use std::f32::consts::E;
 
+type Matrix = Array2<f32>;
+type MatrixTriple = (Matrix, Matrix, Matrix);
+
 /*
 The goal of this file is to demonstrate how the basic functionality of a multi-layer
 neural network is implemented.
@@ -20,19 +23,26 @@ pub fn relu(f: f32) -> f32 {
 
 // This function generates an N layer NN from a set of integers specifying the
 // respective layer sizes in order.
-pub fn init_deep_nn_params(layers: Vec<usize>) -> Result<HashMap<String, Array2<f32>>, String> {
+pub fn init_deep_nn_params(layers: Vec<usize>) -> Result<HashMap<String, Matrix>, String> {
     let mut map = HashMap::new();
 
     for i in 1..layers.len() {
         let this_l = layers[i - 1];
         let next_l = layers[i];
         let w = matrix2d::new_rand(next_l, this_l);
-        let b: Array2<f32> = Array2::zeros((next_l, 1));
+        let b = Array2::<f32>::zeros((next_l, 1));
         map.insert(format!("W{}", i), w);
         map.insert(format!("b{}", i), b);
     }
 
     Ok(map)
+}
+
+// Linear forward is the preceding step to calculating activation.
+pub fn linear_forward(a: Matrix, w: Matrix, b: Matrix) -> (Matrix, MatrixTriple) {
+    let z = w.dot(&a) + &b;
+    // (a, w, b) is the cache tuple
+    (z, (a, w, b))
 }
 
 #[cfg(test)]
@@ -62,5 +72,26 @@ mod tests {
         assert_eq!(nn1["b2"].shape(), [2, 1]);
         assert_eq!(nn1["W3"].shape(), [1, 2]);
         assert_eq!(nn1["b3"].shape(), [1, 1]);
+    }
+
+    #[test]
+    fn test_linear_forward() {
+        let prec = 100000.0;
+        let round = |f: f32| (f * prec).round() / prec;
+
+        let a = ndarray::arr2(&[
+            [1.62434536, -0.61175641],
+            [-0.52817175, -1.07296862],
+            [0.86540763, -2.3015387],
+        ]);
+        let b = ndarray::arr2(&[[1.74481176, -0.7612069, 0.3190391]]);
+        let c = ndarray::arr2(&[[-0.24937038]]);
+
+        let (z, _) = linear_forward(a, b, c);
+        let expected = ndarray::arr2(&[[3.26295337, -1.23429987]]);
+
+        assert_eq!(expected.shape(), z.shape());
+        assert_eq!(round(expected[[0, 0]]), round(z[[0, 0]]));
+        assert_eq!(round(expected[[0, 1]]), round(z[[0, 1]]));
     }
 }
